e66f8b1dc9693d8669b8c167e789a3db1cd4b1e7
saiteja2880 <127845016+saiteja2880@users.noreply.github.com>
2023-04-05 15:40:28 +0530

    Update palendrome.py

--- palendrome.py
+++ palendrome.py
@@ -1,12 +1,16 @@
-def isPalindrome(s):
-    return s == s[::-1]
+def isPalindrome(str):
   
+    # Run loop from 0 to len/2
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
   
-# Driver code
+# main function
 s = "malayalam"
 ans = isPalindrome(s)
   
-if ans:
+if (ans):
     print("Yes")
 else:
     print("No")
